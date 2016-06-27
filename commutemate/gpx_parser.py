c362b31a0a33b15d9484a08605362b448aed0faa
Luis Cipriani <lfcipriani@gmail.com>
2016-06-27 18:58:33 +0200

    exporting track as Ride with Geo Points

--- commutemate/gpx_parser.py
+++ commutemate/gpx_parser.py
@@ -1,4 +1,5 @@
 import gpxpy
+from commutemate.ride import *
 
 class GpxParser(object):
 
@@ -6,4 +7,42 @@ class GpxParser(object):
         gpx_file = open(filename, 'r')
         self.__gpx = gpxpy.parse(gpx_file)
 
+    def get_ride_from_track(self):
+        previous_point = None
+        ride = Ride()
 
+        for track in self.__gpx.tracks:
+            for segment in track.segments:
+                points = segment.points
+
+                for i in range(0, len(points)):
+                    previous = points[i - 1] if i > 0 else points[0]
+                    point    = points[i]
+
+                    timedelta = point.time - previous.time
+                    distance  = self.__distance(point, previous)
+                    speed_kmh = self.__speed(distance, timedelta)
+
+                    ride.add_point(
+                            GeoPoint(point.latitude, 
+                                     point.longitude, 
+                                     point.elevation, 
+                                     speed_kmh, 
+                                     gpxpy.utils.total_seconds(timedelta)))
+
+        return ride
+
+    def __distance(self, point, previous):
+        if point.elevation and previous.elevation:
+            distance = point.distance_3d(previous)
+        else:
+            distance = point.distance_2d(previous)
+        return distance
+
+    def __speed(self, distance, timedelta):
+        seconds = gpxpy.utils.total_seconds(timedelta)
+        speed_kmh = 0
+        if seconds > 0:
+            speed_kmh = (distance / 1000.) / (seconds / 60. ** 2)
+        return speed_kmh
+        
--- tests/gpx_parser_test.py
+++ tests/gpx_parser_test.py
@@ -10,3 +10,12 @@ class TestGpxParser:
     @raises(IOError)
     def test_loading_non_existent_gpx_file(self):
         gp = GpxParser('tests/data/nonexistent.gpx')
+
+    def test_get_ride_from_track(self):
+        gp = GpxParser('tests/data/sample_with_stop.gpx')
+        ride = gp.get_ride_from_track()
+
+        eq_(ride.point_count(), 919)
+        ok_(ride.points[34].speed > 0)
+        ok_(ride.points[34].seconds_from_previous > 0)
+    
