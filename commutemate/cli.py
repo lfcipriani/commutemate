557061ab0d8af65cd941fe9b431dc55c319210b9
Luis Cipriani <lfcipriani@gmail.com>
2016-07-01 00:34:47 +0200

    clustering implemented using DBSCAN

--- commutemate/cli.py
+++ commutemate/cli.py
@@ -3,6 +3,7 @@ import logging as l
 from optparse import OptionParser
 from commutemate.gpx_parser import GpxParser
 from commutemate.detectors import *
+from commutemate.roi import *
 
 class CommutemateCLI(object):
     
@@ -62,7 +63,45 @@ class CommutemateCLI(object):
                 self.__save_json(os.path.join(self.output_folder, "poi_%s.json" % s.id), s.to_JSON())
 
         l.info("Done! There was %d stops detected\nThe data is available at %s" % (total, self.output_folder))
-                
+
+    def clusterize(self):
+        import numpy
+        from sklearn.cluster import DBSCAN
+        from scipy.spatial.distance import pdist, squareform
+        # Getting all json files in specified folder
+        json_files = []
+        for f in os.listdir(self.input_folder):
+            if f.endswith('.json'):
+                json_files.append(os.path.join(self.input_folder, f))
+        l.info("There's %d POI(s) to be clusterized. Preparing data..." % len(json_files))
+
+        # Preparing data
+        geo_coords = []
+        for jsf in json_files:
+            poi = self.__load_json(jsf, PointOfInterest)
+            geo_coords.append([poi.point.lat, poi.point.lon])
+        X = numpy.array(geo_coords)
+        Y = numpy.radians(X) # this is the input of scikit Haversine distance formula
+
+        # Running DBSCAN cluster algorithm
+        # http://scikit-learn.org/stable/modules/clustering.html#dbscan
+        DB_EPS = 0.005 / 6372 # Haversine outputs without considering Earth radius
+        DB_MIN_SAMPLES = 5
+        DB_METRIC = 'haversine'
+        l.info("Running DBSCAN with eps=%0.15f, min_samples=%d, metric=%s" % (DB_EPS, DB_MIN_SAMPLES, DB_METRIC))
+
+        db = DBSCAN(eps=DB_EPS, min_samples=DB_MIN_SAMPLES, metric=DB_METRIC).fit(Y)
+
+        l.info("Done!")
+
+        labels = db.labels_
+        n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        l.info('Estimated number of clusters: %d' % n_clusters)
+
+        print(db.labels_)
+        print(len(db.core_sample_indices_))
+        print(len(db.components_))
+
     def __get_path(self, folder):
         return os.path.abspath(os.path.join(os.getcwd(), folder))
 
@@ -70,6 +109,14 @@ class CommutemateCLI(object):
         outputFile = open(filename, "w")
         outputFile.write(content)
         outputFile.close()
+
+    def __load_json(self, filename, class_to_deserialize=None):
+        f  = open(filename, "r")
+        js = f.read()
+        if class_to_deserialize:
+            js = class_to_deserialize.from_JSON(js)
+        f.close()
+        return js
 
 def main():
     CommutemateCLI()
