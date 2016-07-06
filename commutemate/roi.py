f0784a49422ee083074c372c139af665988e0a4e
Luis Cipriani <lfcipriani@gmail.com>
2016-07-06 17:12:15 +0200

    RegionOfInterest object and sample POIs as json as fixtures

--- commutemate/roi.py
+++ commutemate/roi.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 import hashlib
 from commutemate.ride import GeoPoint
 import commutemate.utils as utils
@@ -77,3 +77,64 @@ class PointOfInterest(object):
         s += "speed_since_previous_stop=%2.6f kmh, " % (self.speed_since_previous_stop)
         s += "duration=%d)" % (self.duration)
         return s
+
+class RegionOfInterest(object):
+
+    CORE = 'core'
+    NON_CORE = 'non_core'
+
+    def __init__(self):
+        self.poi_ids = { RegionOfInterest.CORE: None, RegionOfInterest.NON_CORE: None }
+        self.poi_list = { RegionOfInterest.CORE: None, RegionOfInterest.NON_CORE: None }
+        self.poi_coords = { RegionOfInterest.CORE: None, RegionOfInterest.NON_CORE: None }
+        self.center_range = None
+
+    def set_poi_list(self, poi_list, type_):
+        self.poi_list[type_] = poi_list
+        self.set_poi_ids([p.id for p in poi_list], type_)
+        self.set_poi_coords([[p.point.lat, p.point.lon] for p in poi_list], type_)
+
+    def set_poi_ids(self, poi_ids, type_):
+        self.poi_ids[type_] = poi_ids
+
+    def set_poi_coords(self, poi_coords, type_):
+        self.poi_coords[type_] = poi_coords
+        if type_ == RegionOfInterest.CORE:
+            self.__generate_center_range(poi_coords)
+        
+    def __generate_center_range(self, coords):
+        self.center_range = utils.geo_range_from_center(coords)
+
+    def to_dict(self):
+        js = {
+            "poi_ids": self.poi_ids,
+            "poi_coords": self.poi_coords,
+            "center_range": self.center_range
+        }
+        return js
+
+    def to_JSON(self):
+        return json.dumps(self.to_dict(), indent=4)
+
+    @staticmethod
+    def from_dict(json_dict):
+        roi = RegionOfInterest()
+        for type_ in [RegionOfInterest.CORE,RegionOfInterest.NON_CORE]:
+            roi.set_poi_ids(json_dict["poi_ids"][type_], type_)
+            roi.set_poi_coords(json_dict["poi_coords"][type_], type_)
+        return roi
+
+    @staticmethod
+    def from_JSON(json_str):
+        js = json.loads(json_str)
+        return RegionOfInterest.from_dict(js)
+
+
+    @staticmethod
+    def hydrate_POIs(roi, json_base_path):
+        for type_ in [RegionOfInterest.CORE,RegionOfInterest.NON_CORE]:
+            result = []
+            for i in roi.poi_ids[type_]:
+                result.append(utils.load_json(os.path.join(json_base_path, ("poi_%s.json" % i)),PointOfInterest))
+            roi.set_poi_list(result, type_)
+
--- tests/roi_test.py
+++ tests/roi_test.py
@@ -1,7 +1,7 @@
 from nose.tools import *
 from commutemate.detectors import *
 from commutemate.gpx_parser import GpxParser
-from commutemate.roi import PointOfInterest
+from commutemate.roi import PointOfInterest, RegionOfInterest
 
 class TestPOI:
 
@@ -24,3 +24,47 @@ class TestPOI:
         eq_(stop_POI.duration, 19)
         eq_(stop_POI.previous_stop, 'ce41ef2b46075724919fa8b6579dd144')
         eq_(stop_POI.poi_type, PointOfInterest.TYPE_STOP)
+
+class TestROI:
+
+    def test_ROI_hydration(self):
+        roi = RegionOfInterest()
+        poi_ids_core = ['1edf2a64bf9973b4bd774c8f4d073895','1edd013cf467235ad09288c37c07190f','1ec75e917573fbc0896a7082f514209f']
+        poi_ids_non_core = ['1ead3fd8056b691e1653c888e29e8e7d']
+        roi.set_poi_ids(poi_ids_core, RegionOfInterest.CORE)
+        roi.set_poi_ids(poi_ids_non_core, RegionOfInterest.NON_CORE)
+
+        ok_(not roi.center_range)
+        ok_(not roi.poi_list[RegionOfInterest.CORE])
+        
+        RegionOfInterest.hydrate_POIs(roi, 'tests/data/')
+
+        eq_(int(roi.center_range[2]), 1761)
+        eq_(type(roi.poi_list[RegionOfInterest.CORE][0]), PointOfInterest)
+        eq_(len(roi.poi_list[RegionOfInterest.CORE]), 3)
+        eq_(len(roi.poi_list[RegionOfInterest.NON_CORE]), 1)
+        eq_(len(roi.poi_coords[RegionOfInterest.CORE]), 3)
+        eq_(len(roi.poi_coords[RegionOfInterest.NON_CORE]), 1)
+
+    def test_roi_json_serialization(self):
+        roi = RegionOfInterest()
+        poi_ids_core = ['1edf2a64bf9973b4bd774c8f4d073895','1edd013cf467235ad09288c37c07190f','1ec75e917573fbc0896a7082f514209f']
+        poi_ids_non_core = ['1ead3fd8056b691e1653c888e29e8e7d']
+        roi.set_poi_ids(poi_ids_core, RegionOfInterest.CORE)
+        roi.set_poi_ids(poi_ids_non_core, RegionOfInterest.NON_CORE)
+        RegionOfInterest.hydrate_POIs(roi, 'tests/data/')
+
+        js = roi.to_JSON()
+        new_roi = RegionOfInterest.from_JSON(js)
+
+        eq_(int(new_roi.center_range[2]), 1761)
+        ok_(not new_roi.poi_list[RegionOfInterest.CORE])
+
+        RegionOfInterest.hydrate_POIs(new_roi, 'tests/data/')
+
+        eq_(int(new_roi.center_range[2]), 1761)
+        eq_(type(new_roi.poi_list[RegionOfInterest.CORE][0]), PointOfInterest)
+        eq_(len(new_roi.poi_list[RegionOfInterest.CORE]), 3)
+        eq_(len(new_roi.poi_list[RegionOfInterest.NON_CORE]), 1)
+        eq_(len(new_roi.poi_coords[RegionOfInterest.CORE]), 3)
+        eq_(len(new_roi.poi_coords[RegionOfInterest.NON_CORE]), 1)
